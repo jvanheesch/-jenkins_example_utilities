ea530a4bd1cd5edc325f76acb483e87cec194d28
Joris Van Heesch <joris.vanheesch@dxcfds.com>
2018-06-26 11:15:14 +0200

    groovy

--- src/com/github/jvanheesch/HelperGroovy.groovy
+++ src/com/github/jvanheesch/HelperGroovy.groovy
@@ -1,23 +1,6 @@
 package com.github.jvanheesch
 
 class HelperGroovy {
-    static def mvn(script, args) {
-        script.sh "${script.tool 'Maven'}/bin/mvn -s ${script.env.HOME}/jenkins.xml -o ${args}"
-    }
-
-    static def publishTestResults(script) {
-        script.junit '**/target/surefire-reports/*.xml'
-    }
-
-    static def checkoutProject(script) {
-        script.git branch: '20180618.testing.jenkins', changelog: false, poll: false, url: 'git@github.com:jvanheesch/util.git'
-    }
-
-    static def test(script) {
-        script.sh "echo 'test1'"
-        script.echo "working"
-    }
-
     static def testUtilities(script) {
         script.echo "testUtilities"
     }
